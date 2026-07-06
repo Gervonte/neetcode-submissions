5bc7f54ea3e1542e1abb52b799f696517b48897e
Gervonté Fowler <hello@builtbygervonte.com>
2026-07-06 16:41:58 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,35 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        const newArr = [...this.stack]
+        return newArr.sort((a,b)=>a-b)[0];
+    }
+}
